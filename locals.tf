e2c6c4fdb05a6232d2252a27d463f55d713bd6b8
Craig Vitter <craig.vitter@gmail.com>
2024-10-03 12:57:49 -0400

    first commit

--- locals.tf
+++ locals.tf
@@ -2,7 +2,7 @@ locals {
   buckets = {
     "aws" : aws_s3_bucket.cur_bucket.bucket
   }
-  s3_bucket_name = "${var.customer_prefix}-stacklet-shared-cur"
+  s3_bucket_name = "${var.customer_prefix}-stacklet-shared-cur-craig"
 }
 
 data "aws_region" "current" {}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+stacklet_saas_account_id = "919639712056"
+customer_prefix = "demo"
